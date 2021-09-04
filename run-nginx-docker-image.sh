63ee1fa07ac086d34a6b18157cedad0918d5d854
Anton Shvetsov <anton.shvetsov@mail.ru>
2021-09-04 18:00:00 +0300

    change basic page

--- run-nginx-docker-image.sh
+++ run-nginx-docker-image.sh
@@ -1,4 +1,4 @@
 docker run -it -p 80:80 \
-    -v /$PWD/dist/angular-nginx-docker://usr/share/nginx/html:ro \
+    -v /$PWD/dist/post-editor://usr/share/nginx/html:ro \
     -v /$PWD/.nginx/nginx.conf://etc/nginx/nginx.conf:ro \
     nginx:alpine
